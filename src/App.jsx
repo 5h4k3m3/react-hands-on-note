1ca125772e1a9e63a69a4dfae9f36dce41a01f8b
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-26 22:23:26 +0900

    ✨ select first note when start

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,10 @@ export const App = () => {
     localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
 
+  useEffect(() => {
+    setActiveNote(notes[0].id);
+  }, []);
+
   const onAddNote = () => {
     const newNote = {
       id: uuid(),
