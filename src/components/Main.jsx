2381ff0fba97a3b07fccf5b9dca8d402eb5c332b
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-26 21:51:10 +0900

    ✨ impl main html css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,5 +2,17 @@ import React from "react";
 import "./Main.css";
 
 export const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="appMain">
+      <div className="appMainNoteEdit">
+        <input type="text" />
+        <textarea id="" placeholder="Edit Note"></textarea>
+      </div>
+      <div className="appMainNotePreview">
+        <h1 className="previewTitle">Title</h1>
+        <div className="markdownPreview"></div>
+        <div className="notePreview"></div>
+      </div>
+    </div>
+  );
 };
