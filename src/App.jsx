6074ce8534f0830b70587f678900cc6e98cfbbe2
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-26 21:57:24 +0900

    ✨ export selected note to preview

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,10 @@ export const App = () => {
     setNotes(filterNotes);
   };
 
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+  };
+
   return (
     <div className="App">
       <Sidebar
@@ -32,7 +36,7 @@ export const App = () => {
         activeNote={activeNote}
         setActiveNote={setActiveNote}
       />
-      <Main />
+      <Main activeNote={getActiveNote()} />
     </div>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Main.css";
 
-export const Main = () => {
+export const Main = ({ activeNote }) => {
+  if (!activeNote) {
+    return <p className="noActiveNote">Select Note</p>;
+  }
   return (
     <div className="appMain">
       <div className="appMainNoteEdit">
@@ -9,9 +12,8 @@ export const Main = () => {
         <textarea id="" placeholder="Edit Note"></textarea>
       </div>
       <div className="appMainNotePreview">
-        <h1 className="previewTitle">Title</h1>
-        <div className="markdownPreview"></div>
-        <div className="notePreview"></div>
+        <h1 className="previewTitle">{activeNote.title}</h1>
+        <div className="markdownPreview">{activeNote.content}</div>
       </div>
     </div>
   );
